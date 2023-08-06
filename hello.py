cbc5158121fd6bb0e58a556b9a6b5bc098fcaeb2
EC2 Default User <ec2-user@ip-172-31-40-175.ap-southeast-1.compute.internal>
2023-08-06 05:11:18 +0000

    added functions and tests

--- hello.py
+++ hello.py
@@ -2,6 +2,31 @@ def add(x, y):
     """This is an add function"""
 
     return x + y
+    
+    
+def multiply(x, y):
+    """
+    This is a multiply function
+    """
+    
+    return x * y
+    
+def subtract(from_num, by_num):
+    """
+    This is a subtract function, to subtract from from_num by by_num
+    """
+    
+    return from_num - by_num
+
+def divide(from_num, by_num):
+    """
+    This is a divide function, to divide from_num by by_num
+    """
+    
+    return from_num / by_num
 
 
 print(add(1, 1))
+print(multiply(2,3))
+print(subtract(10,2))
+print(divide(20,5))
--- test_hello.py
+++ test_hello.py
@@ -1,5 +1,17 @@
-from hello import add
+from hello import add, multiply, subtract, divide
 
 
 def test_add():
     assert 2 == add(1, 1)
+    
+    
+def test_multiply():
+    assert 10 == multiply(2, 5)
+    
+    
+def test_subtract():
+    assert 3 == subtract(9,6)
+    
+
+def test_divide():
+    assert 10 == divide(100,10)
